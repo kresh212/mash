12507bdbf9585bf487ec5f830d1fd72dfe996d2f
aadudyrev <aadudyrev@gmail.com>
2017-08-05 23:38:06 +0500

    Initial Commit

---
+++ MachinariumGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MachinariumGame
+//
+//  Created by Александр Дудырев on 05.08.17.
+//  Copyright © 2017 Александр Дудырев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MachinariumGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MachinariumGame
+//
+//  Created by Александр Дудырев on 05.08.17.
+//  Copyright © 2017 Александр Дудырев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
